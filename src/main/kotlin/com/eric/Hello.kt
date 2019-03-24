8cda667cf74f8f6240c757f6951fe2a391a139a5
Shiheric0925 <eric84925@gmail.com>
2019-03-24 20:55:56 +0800

    2019/03/24-1

--- src/main/kotlin/com/eric/Hello.kt
+++ src/main/kotlin/com/eric/Hello.kt
@@ -1,5 +1,12 @@
 package com.eric
 
 fun main(args: Array<String>) {
-    println("hello kkk")
+//    println("hello kkk")
+    val h=Human()
+    h.hello()
+}
+class Human{
+    fun hello(){
+        println("hello kkkkk")
+    }
 }
